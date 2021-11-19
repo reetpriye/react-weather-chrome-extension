a9c042d6073e4f6cd5a530ffeb281f6e7a2d53d7
Reet Priye <reetpriye@gmail.com>
2021-11-19 09:29:33 +0530

    Initial commit

---
+++ src/popup/popup.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react'
+import { render } from 'react-dom'
+
+import './popup.css'
+
+const App: React.FC<{}> = () => {
+  return (
+    <div>
+      <img src='icon.png' alt='icon' />
+    </div>
+  )
+}
+
+const root = document.createElement('div')
+document.body.appendChild(root)
+
+render(<App />, root)
